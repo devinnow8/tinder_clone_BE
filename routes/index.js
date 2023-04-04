a46c592cffea72d5ef78b28284cdb66b0fa4ba88
Mayank sharma <msharma@innow8apps.com>
2023-04-04 14:32:53 +0530

    updates

--- routes/index.js
+++ routes/index.js
@@ -4,66 +4,69 @@ const User = require("../models/user")
 
 const routes = (app) => {
 
-  // get user by phone number
-  app.get('/getUser', (req, res) => {
-    console.log("innnnnn")
 
-    if(!req.body.phoneNumber){
-      res.send({status: 500, error: "Validation error"})
-    }
-  User.find({ phoneNumber: req.body.phoneNumber}).then((users) => {
-    res.send({status: 200, data: users})
-  }).catch((error) => {
-   res.send({status: 500, error})
+  // get user by id
+  app.get('/getUser/:id', (req, res) => {
+    User.find({ _id: req.params.id || "" }).then((users) => {
+      res.send({ status: 200, data: users })
+    }).catch((error) => {
+      res.send({ status: 500, error })
+    })
   })
-})
+
+  // get user by phone number
+  app.get('/getUsers', (req, res) => {
+    User.find({ phoneNumber: req.query.phoneNumber || "" }).then((users) => {
+      res.send({ status: 200, data: users })
+    }).catch((error) => {
+      res.send({ status: 500, error })
+    })
+  })
 
   // create user
-   app.post('/createUser', (req, res) => {
-    console.log("innnnnn")
-
-      if(!req.body.name || 
-        !req.body.email || 
-        !req.body.age || 
-        !req.body.dob || 
-        !req.body.phoneNumber || 
-        !req.body.gender || 
-        !req.body.includeMeInSearch || 
-        !req.body.sexualOrientation ||
-        !req.body.interestedIn ||
-        !req.body.lookingFor ||
-        !req.body.school ||
-        !req.body.passions ||
-        !req.body.photos ||
-        !req.body.currentLocation ||
-        !req.body.isNotificationOn ){
-          return res.send({status: 500, error: "Validation error"})
-        }
-      let newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        age: req.body.age,
-        dob: req.body.dob,
-        phoneNumber: req.body.phoneNumber,
-        gender: req.body.gender,
-        isShowGender: req.body.isShowGender,
-        includeMeInSearch: req.body.includeMeInSearch,
-        sexualOrientation: req.body.sexualOrientation,
-        isShowOrientation: req.body.isShowOrientation,
-        interestedIn: req.body.interestedIn,
-        lookingFor: req.body.lookingFor,
-        school: req.body.school,
-        passions: req.body.passions,
-        photos: req.body.photos,
-        currentLocation: req.body.currentLocation,
-        isNotificationOn: req.body.isNotificationOn
-      })
-      newUser.save().then((user) => {
-        res.send({status: 200, data: user})
-       }).catch((error) => {
-        res.send({status: 500, error})
-       })
-      })
+  app.post('/createUser', (req, res) => {
+    if (!req.body.name ||
+      !req.body.email ||
+      !req.body.age ||
+      !req.body.dob ||
+      !req.body.phoneNumber ||
+      !req.body.gender ||
+      !req.body.includeMeInSearch ||
+      !req.body.sexualOrientation ||
+      !req.body.interestedIn ||
+      !req.body.lookingFor ||
+      !req.body.school ||
+      !req.body.passions ||
+      !req.body.photos ||
+      !req.body.currentLocation ||
+      !req.body.isNotificationOn) {
+      return res.send({ status: 500, error: "Validation error" })
+    }
+    let newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      age: req.body.age,
+      dob: req.body.dob,
+      phoneNumber: req.body.phoneNumber,
+      gender: req.body.gender,
+      isShowGender: req.body.isShowGender,
+      includeMeInSearch: req.body.includeMeInSearch,
+      sexualOrientation: req.body.sexualOrientation,
+      isShowOrientation: req.body.isShowOrientation,
+      interestedIn: req.body.interestedIn,
+      lookingFor: req.body.lookingFor,
+      school: req.body.school,
+      passions: req.body.passions,
+      photos: req.body.photos,
+      currentLocation: req.body.currentLocation,
+      isNotificationOn: req.body.isNotificationOn
+    })
+    newUser.save().then((user) => {
+      res.send({ status: 200, data: user })
+    }).catch((error) => {
+      res.send({ status: 500, error })
+    })
+  })
 
   // update user  by phone number
   app.post('/updateUser/:id', (req, res) => {
@@ -72,9 +75,9 @@ const routes = (app) => {
         message: "Data to update can not be empty!",
       });
     }
-  
+
     const id = req.params.id;
-  
+
     User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
       .then((data) => {
         if (!data) {
@@ -89,6 +92,7 @@ const routes = (app) => {
           error: error
         });
       });
-})}
+  })
+}
 
 module.exports = routes

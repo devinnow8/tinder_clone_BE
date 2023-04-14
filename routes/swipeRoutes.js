5b0c92b4458f9cbd9f02ea0aee03e883a2dd112b
Ashutosh Sharma <ashutoshsharma@ninjacart.com>
2023-04-14 15:12:11 +0530

    fix: minor fix

--- routes/swipeRoutes.js
+++ routes/swipeRoutes.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const {
-  like,
-  dislike,
   likeSwipe,
   dislikeSwipe
 } = require("../controllers/swipesController");
 
 const swipes = express()
 
-swipes.post("/like", like);
 swipes.post("/noAuth/likeSwipe", likeSwipe);
 swipes.post("/noAuth/dislikeSwipe", dislikeSwipe);   // added noAuth temporary for testing 
-
-swipes.post("/dislike", dislike);
 
 module.exports = swipes;
